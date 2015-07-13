a6d91f0f8107ed8dc9a97ffe62350a90af80e618
vonors <ibda3tetwer@hotmail.fr>
2015-07-13 22:46:46 +0000

    Create arbahh.js

---
+++ arbahh.js
@@ -0,0 +1,6 @@
+if (typeof jQuery == 'undefined')
+{
+	document.write('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>');
+}
+
+	document.write('<script type="text/javascript" src="http://traidnt.net/api/arbah_api.js"></script>');
